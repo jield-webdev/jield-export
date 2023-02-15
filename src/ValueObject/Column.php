cef86b55bb72747023d0e5219037d8e074940b0a
Johan van der Heide <info@jield.nl>
2023-02-15 16:29:35 +0100

    WIP on setting update module

--- src/ValueObject/Column.php
+++ src/ValueObject/Column.php
@@ -6,6 +6,11 @@ namespace Jield\Export\ValueObject;
 
 use codename\parquet\data\DataColumn;
 use codename\parquet\data\DataField;
+use codename\parquet\data\DateTimeDataField;
+use DateTime;
+use DateTimeImmutable;
+use DateTimeInterface;
+use Jield\Export\Helper\TextHelpers;
 use Webmozart\Assert\Assert;
 
 final class Column
@@ -13,6 +18,8 @@ final class Column
     public const TYPE_STRING = 'string';
     public const TYPE_INTEGER = 'integer';
     public const TYPE_DATE = 'date';
+    public const TYPE_TIME = 'time';
+    public const TYPE_BOOLEAN = 'boolean';
 
     public array $types = [
         self::TYPE_STRING,
@@ -22,21 +29,48 @@ final class Column
 
     public function __construct(
         private readonly string $columnName,
-        private readonly string $type = self::TYPE_STRING,
+        private string $type = self::TYPE_STRING,
         private array $data = []
     ) {
         Assert::inArray($type, $this->types);
     }
 
-    public function addRow(int|string|float $data): void
+    public function addRow(int|string|float|null|bool|DateTime|DateTimeImmutable $data): void
     {
+        switch ($this->type) {
+            case self::TYPE_INTEGER:
+                Assert::integer($data);
+                break;
+            case self::TYPE_STRING:
+                $data = TextHelpers::beautifyTextValue($data);
+                break;
+            case self::TYPE_BOOLEAN:
+                Assert::boolean($data);
+                $data       = $data ? 1 : null;
+                $this->type = self::TYPE_INTEGER; //Set to integer
+                break;
+            case self::TYPE_DATE:
+                Assert::isInstanceOf($data, DateTimeInterface::class);
+                $data = $data instanceof DateTime ? $data : DateTime::createFromImmutable($data);
+                break;
+            case self::TYPE_TIME:
+                Assert::implementsInterface($data, DateTimeInterface::class);
+                $data = $data->format('H:i');
+                break;
+        }
         $this->data[] = $data;
     }
 
     public function toParquetColumn(): DataColumn
     {
+        if ($this->type === self::TYPE_DATE) {
+            $field = DateTimeDataField::create(name: $this->columnName, format: 4);
+        } else {
+            $field = DataField::createFromType(name: $this->columnName, type: $this->type);
+        }
+
         return new DataColumn(
-            field: DataField::createFromType(name: $this->columnName, type: $this->type),
+            field: $field,
             data: $this->data
         );
     }
