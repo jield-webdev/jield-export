c1d3db0411313670d8f8800a1ecd816cf1371402
Johan van der Heide <info@jield.nl>
2023-03-27 16:43:17 +0200

    Update to support service principles

--- src/Options/ModuleOptions.php
+++ src/Options/ModuleOptions.php
@@ -8,55 +8,7 @@ use Laminas\Stdlib\AbstractOptions;
 
 class ModuleOptions extends AbstractOptions
 {
-    protected string $azureBlobStorageConnectionString = '';
-
-    private string $blobContainer = 'dropzone';
-
-    private string $parquetFolder = 'parquet';
-
-    private string $excelFolder = 'excel';
-
     protected array $entities = [];
-
-    public function getAzureBlobStorageConnectionString(): string
-    {
-        return $this->azureBlobStorageConnectionString;
-    }
-
-    public function setAzureBlobStorageConnectionString(string $azureBlobStorageConnectionString): void
-    {
-        $this->azureBlobStorageConnectionString = $azureBlobStorageConnectionString;
-    }
-
-    public function getBlobContainer(): string
-    {
-        return $this->blobContainer;
-    }
-
-    public function setBlobContainer(string $blobContainer): void
-    {
-        $this->blobContainer = $blobContainer;
-    }
-
-    public function getParquetFolder(): string
-    {
-        return $this->parquetFolder;
-    }
-
-    public function setParquetFolder(string $parquetFolder): void
-    {
-        $this->parquetFolder = $parquetFolder;
-    }
-
-    public function getExcelFolder(): string
-    {
-        return $this->excelFolder;
-    }
-
-    public function setExcelFolder(string $excelFolder): void
-    {
-        $this->excelFolder = $excelFolder;
-    }
 
     public function getEntities(): array
     {
